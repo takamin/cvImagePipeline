7e17c4847f0dae717f9aeca90280265159d3915a
Koji Takami <takami@benest.co.jp>
2014-07-29 16:13:02 +0900

    no process image when input Mat is empty.

--- src/cvImagePipeline/SubMat.cpp
+++ src/cvImagePipeline/SubMat.cpp
@@ -16,6 +16,9 @@ namespace cvImagePipeline {
 		void SubMat::execute() {
 			const Mat& src1 = getInputMat("src1");
 			const Mat& src2 = getInputMat("src2");
+			if (src1.empty() || src2.empty()) {
+				return;
+			}
 			Mat& dst = refOutputMat();
 			cv::subtract(src1, src2, dst);
 		}
