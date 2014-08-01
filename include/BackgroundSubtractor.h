af5ae508f81e723af437aba35a2c31a189716fcf
Koji Takami <takami@benest.co.jp>
2014-08-01 19:57:39 +0900

    add other algorithm

--- include/BackgroundSubtractor.h
+++ include/BackgroundSubtractor.h
@@ -1,16 +1,24 @@
 #pragma once
 #include "ImageProcessor.h"
+#define GMG 0
+#define MOG 1
+#define MOG2 2
+#define _BackgroundSubtractor_ALG MOG
 namespace cvImagePipeline {
 	namespace Filter {
 		class __declspec(dllexport) BackgroundSubtractor : public ImageProcessor
 		{
 		public:
 			DECLARE_CVFILTER;
+			TParam<int> algo;
+			TParam<int> averageCount;
+		public:
 			BackgroundSubtractor();
 			~BackgroundSubtractor();
 			void execute();
 		private:
 			cv::BackgroundSubtractor* backgroundSubtractor;
+			int buffer_count;
 		};
 	}
 }
--- src/cvImagePipeline/BackgroundSubtractor.cpp
+++ src/cvImagePipeline/BackgroundSubtractor.cpp
@@ -4,9 +4,11 @@ namespace cvImagePipeline {
 	namespace Filter {
 		IMPLEMENT_CVFILTER(BackgroundSubtractor);
 		BackgroundSubtractor::BackgroundSubtractor()
-			: backgroundSubtractor(0)
+			: backgroundSubtractor(0), buffer_count(0),
+			algo("algo", GMG), averageCount("averageCount", 120)
 		{
-			backgroundSubtractor = new cv::BackgroundSubtractorGMG();
+			defParam(averageCount);
+			defParam(algo);
 		}
 
 		BackgroundSubtractor::~BackgroundSubtractor()
@@ -17,8 +19,36 @@ namespace cvImagePipeline {
 		void BackgroundSubtractor::execute() {
 			const cv::Mat& frame = getInputMat();
 			if (!frame.empty()) {
+				if (buffer_count < averageCount) {
+					buffer_count++;
+				}
+				if (backgroundSubtractor == 0) {
+					switch (algo) {
+					case GMG:
+						backgroundSubtractor = new cv::BackgroundSubtractorGMG();
+						break;
+					case MOG:
+						backgroundSubtractor = new cv::BackgroundSubtractorMOG(averageCount, 3, 0.0);
+						break;
+					case MOG2:
+						backgroundSubtractor = new cv::BackgroundSubtractorMOG2(averageCount, 50, true);
+						break;
+					}
+				}
+
 				cv::Mat& mask = refOutputMat();
-				((cv::BackgroundSubtractorGMG*)backgroundSubtractor)->operator()(frame, mask);
+				switch (algo) {
+				case GMG:
+					(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor).learningRate = 1.0 / buffer_count;
+					(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor)(frame, mask);
+					break;
+				case MOG:
+					(*(cv::BackgroundSubtractorMOG*)backgroundSubtractor)(frame, mask);
+					break;
+				case MOG2:
+					(*(cv::BackgroundSubtractorMOG2*)backgroundSubtractor)(frame, mask);
+					break;
+				}
 			}
 		}
 	}
