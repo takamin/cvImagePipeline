edff0bfb72b1b00f90f9c20fb1af513c1b73e653
Koji Takami <takami@benest.co.jp>
2014-08-06 19:38:26 +0900

    make BackgroundSubtraction to use a pointer to member function for speed.

--- include/BackgroundSubtractor.h
+++ include/BackgroundSubtractor.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <Windows.h>
+#include <hash_map>
 #include "ImageProcessor.h"
 #define GMG 0
 #define MOG 1
@@ -12,8 +13,10 @@ namespace cvImagePipeline {
 			CRITICAL_SECTION cs;
 		public:
 			DECLARE_CVFILTER;
-			TParam<int> algo;
+			enum Algorithm { AlgorithmGMG = GMG, AlgorithmMOG = MOG, AlgorithmMOG2 = MOG2 };
+			TParam<Algorithm> algorithm;
 			TParam<int> averageCount;
+			TParam<bool> MOG2_shadowDetection;
 		public:
 			BackgroundSubtractor();
 			~BackgroundSubtractor();
@@ -23,10 +26,23 @@ namespace cvImagePipeline {
 		private:
 			cv::BackgroundSubtractor* backgroundSubtractor;
 			int buffer_count;
+#pragma warning(disable:4251)
 			cv::Mat backgroundImage;
+#pragma warning(default:4251)
 		private:
 			void shutdown();
 			void create();
+			void (BackgroundSubtractor::*executeAlgorithm)(const cv::Mat&, cv::Mat& mask);
+			void executeGMG(const cv::Mat& frame, cv::Mat& mask);
+			void executeMOG(const cv::Mat& frame, cv::Mat& mask);
+			void executeMOG2(const cv::Mat& frame, cv::Mat& mask);
 		};
 	}
 }
+__declspec(dllexport) std::istringstream& operator >> (
+	std::istringstream& is,
+	cvImagePipeline::Filter::BackgroundSubtractor::Algorithm& value);
+__declspec(dllexport) std::stringstream& operator << (
+	std::stringstream& ss, 
+	const cvImagePipeline::Filter::BackgroundSubtractor::Algorithm& value);
+
--- src/cvImagePipeline/BackgroundSubtractor.cpp
+++ src/cvImagePipeline/BackgroundSubtractor.cpp
@@ -4,11 +4,14 @@ namespace cvImagePipeline {
 	namespace Filter {
 		IMPLEMENT_CVFILTER(BackgroundSubtractor);
 		BackgroundSubtractor::BackgroundSubtractor()
-			: backgroundSubtractor(0), buffer_count(0),
-			algo("algo", GMG), averageCount("averageCount", 120)
+			: buffer_count(0), executeAlgorithm(0), backgroundSubtractor(0),
+			algorithm("algorithm", AlgorithmMOG2),
+			averageCount("averageCount", 120),
+			MOG2_shadowDetection("MOG2_shadowDetection", true)
 		{
+			defParam(algorithm);
 			defParam(averageCount);
-			defParam(algo);
+			defParam(MOG2_shadowDetection);
 			::InitializeCriticalSection(&cs);
 		}
 
@@ -29,18 +32,7 @@ namespace cvImagePipeline {
 					create();
 				}
 				cv::Mat& mask = refOutputMat();
-				switch (algo) {
-				case GMG:
-					(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor).learningRate = 1.0 / buffer_count;
-					(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor)(frame, mask);
-					break;
-				case MOG:
-					(*(cv::BackgroundSubtractorMOG*)backgroundSubtractor)(frame, mask);
-					break;
-				case MOG2:
-					(*(cv::BackgroundSubtractorMOG2*)backgroundSubtractor)(frame, mask);
-					break;
-				}
+				(this->*executeAlgorithm)(frame, mask);
 				backgroundSubtractor->getBackgroundImage(backgroundImage);
 				::LeaveCriticalSection(&cs);
 			}
@@ -60,17 +52,73 @@ namespace cvImagePipeline {
 		}
 		void BackgroundSubtractor::create() {
 			buffer_count = 0;
-			switch (algo) {
+			switch (algorithm) {
 			case GMG:
 				backgroundSubtractor = new cv::BackgroundSubtractorGMG();
+				executeAlgorithm = &BackgroundSubtractor::executeGMG;
 				break;
 			case MOG:
 				backgroundSubtractor = new cv::BackgroundSubtractorMOG(averageCount, 3, 0.0);
+				executeAlgorithm = &BackgroundSubtractor::executeMOG;
 				break;
 			case MOG2:
-				backgroundSubtractor = new cv::BackgroundSubtractorMOG2(averageCount, 50, true);
+				backgroundSubtractor = new cv::BackgroundSubtractorMOG2(averageCount, 50, MOG2_shadowDetection);
+				executeAlgorithm = &BackgroundSubtractor::executeMOG2;
 				break;
+			}
+		}
+		void BackgroundSubtractor::executeGMG(const cv::Mat& frame, cv::Mat& mask) {
+			(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor).learningRate = 1.0 / buffer_count;
+			(*(cv::BackgroundSubtractorGMG*)backgroundSubtractor)(frame, mask);
+		}
+		void BackgroundSubtractor::executeMOG(const cv::Mat& frame, cv::Mat& mask) {
+			(*(cv::BackgroundSubtractorMOG*)backgroundSubtractor)(frame, mask);
+		}
+		void BackgroundSubtractor::executeMOG2(const cv::Mat& frame, cv::Mat& mask) {
+			(*(cv::BackgroundSubtractorMOG2*)backgroundSubtractor)(frame, mask);
+			if (MOG2_shadowDetection) {
+				cv::threshold(mask, mask, 150, 255, CV_THRESH_BINARY);
 			}
 		}
 	}
 }
+using namespace cvImagePipeline::Filter;
+std::istringstream& operator >> (
+	std::istringstream& is,
+	BackgroundSubtractor::Algorithm& value)
+{
+	std::string str;
+	is >> str;
+	if (str == "AlgorithmGMG") {
+		value = BackgroundSubtractor::AlgorithmGMG;
+	}
+	else if (str == "AlgorithmMOG") {
+		value = BackgroundSubtractor::AlgorithmMOG;
+	}
+	else if (str == "AlgorithmMOG2") {
+		value = BackgroundSubtractor::AlgorithmMOG2;
+	}
+	return is;
+}
+
+
+std::stringstream& operator << (
+	std::stringstream& ss,
+	const BackgroundSubtractor::Algorithm& value)
+{
+	std::string str;
+	switch (value) {
+	case BackgroundSubtractor::AlgorithmGMG:
+		str = "AlgorithmGMG";
+		break;
+	case BackgroundSubtractor::AlgorithmMOG:
+		str = "AlgorithmMOG";
+		break;
+	case BackgroundSubtractor::AlgorithmMOG2:
+		str = "AlgorithmMOG2";
+		break;
+	}
+	ss << str;
+	return ss;
+}
+
